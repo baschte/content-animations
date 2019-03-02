26d921d747a51d1bbc17e5b7da656d5bd84873ca
Sebastian Richter <sebastian.richter@pluswerk.ag>
2019-03-02 18:30:05 +0100

    [CLEANUP] remove unnecessary stuff

--- ext_localconf.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
-}
-
-// register signal slot for adding necessary TCA fields to really all elements of this TYPO3 instance
-$signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
-$signalSlotDispatcher->connect(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::class, 'tcaIsBeingBuilt', \Baschte\ContentAnimations\Slots\ExtTablesInclusionPostProcessing::class, 'processData');
