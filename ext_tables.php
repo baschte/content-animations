e050949f578e9657d0d4dafb95dbe9ff200e2349
jmverges <juanmanuel@vergessolanas.es>
2019-03-21 12:04:09 +0100

    [FEATURE] Toggle to shutdown the automatic append of Animation tab to all CTypes

--- ext_tables.php
+++ ext_tables.php
@@ -4,11 +4,19 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-// add animation tab to all ctypes
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', '
+$extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+    \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
+);
+
+$packageConfiguration = $extensionConfiguration->get('content_animations');
+
+if (empty($packageConfiguration['disableAddAnimationsTab'])) {
+    // add animation tab to all ctypes
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_content', '
     --div--;LLL:EXT:content_animations/Resources/Private/Language/locallang_be.xlf:tab.animation,
     --palette--;LLL:EXT:content_animations/Resources/Private/Language/locallang_be.xlf:palette.animation-settings;
         tx_content_animations_animation,
     --palette--;LLL:EXT:content_animations/Resources/Private/Language/locallang_be.xlf:palette.timing-settings;
         tx_content_animations_timing'
-);
+    );
+}
