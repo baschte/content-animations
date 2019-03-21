1aa9337f1e35ed085ea899165badf5d2492f1900
Sebastian Richter <sebastian.richter@pluswerk.ag>
2019-03-21 19:46:41 +0100

    [!!!][TASK] restructuring ts includes and layouts for thirdparty extensions

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -4,6 +4,8 @@ if (!defined('TYPO3_MODE')) {
 }
 
 // add static typoscript includes
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('content_animations', 'Configuration/TypoScript/BootstrapPackage', 'Content Animations: Bootstrap Package v10.x');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('content_animations', 'Configuration/TypoScript/BootstrapPackage/v10', 'Content Animations: Bootstrap Package v10.x');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('content_animations', 'Configuration/TypoScript/BootstrapPackage/v9', 'Content Animations: Bootstrap Package v9.x');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('content_animations', 'Configuration/TypoScript/BootstrapPackage/v8', 'Content Animations: Bootstrap Package v8.x');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('content_animations', 'Configuration/TypoScript/FluidStyledContent', 'Content Animations: Fluid Styled Content');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('content_animations', 'Configuration/TypoScript/HigherEducationPackage', 'Content Animations: Higher Education Package v9.x');
