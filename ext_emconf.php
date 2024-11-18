7d9648d5d8154b736e8570828ab0fcfe0a8f1fdc
Sebastian Richter <info@baschte.de>
2024-11-18 21:53:43 +0100

    [TASK] bump to latest TYPO3 13 version

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.5.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.99-13.1.99',
+            'typo3' => '12.4.99-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
