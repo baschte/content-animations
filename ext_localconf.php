e3a60fd86332f1fe34a5f53f550162d3d6afc7e0
Stephan Kellermayr <stephan.kellermayr@gmail.com>
2025-10-05 15:12:27 +0200

    Wrap content of ext_localconf.php with call_user_func

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,26 +4,28 @@ if (!defined('TYPO3_MODE') && !defined('TYPO3')) {
     die ('Access denied.');
 }
 
-// register own renderType
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1552428667] = [
-    'nodeName' => 'animationPreview',
-    'priority' => '40',
-    'class' => \Baschte\ContentAnimations\Form\Elements\AnimationPreviewField::class
-];
+call_user_func(static function () {
+    // register own renderType
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1552428667] = [
+        'nodeName' => 'animationPreview',
+        'priority' => '40',
+        'class' => \Baschte\ContentAnimations\Form\Elements\AnimationPreviewField::class
+    ];
 
-// register own typoscript FILECONTENT cObject (can be removed once v11 suppot is dropped)
-if(!isset($GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects']['FILECONTENT'])) {
-	$GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects'] = array_merge(
-		$GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects'] ?? [],
-		['FILECONTENT' => \Baschte\ContentAnimations\ContentObject\FileContentContentObject::class]
-	);
-}
+    // register own typoscript FILECONTENT cObject (can be removed once v11 suppot is dropped)
+    if (!isset($GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects']['FILECONTENT'])) {
+        $GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects'] = array_merge(
+            $GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects'] ?? [],
+            ['FILECONTENT' => \Baschte\ContentAnimations\ContentObject\FileContentContentObject::class]
+        );
+    }
 
-// get extensionConfiguration for 'content_animations'
-$extensionManagementUtility = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class);
-$extensionConfiguration = $extensionManagementUtility->get('content_animations');
+    // get extensionConfiguration for 'content_animations'
+    $extensionManagementUtility = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class);
+    $extensionConfiguration = $extensionManagementUtility->get('content_animations');
 
-// register footer preview to tt_content_drawFooter hook if this feature is active
-if (empty($extensionConfiguration['hideFooterAnimationLabel']) || !$extensionConfiguration['hideFooterAnimationLabel']) {
-    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('templates.typo3/cms-backend.partialsRootPaths = baschte/content-animations:Resources/Private/TemplateOverrides/typo3/cms-backend');
-}
+    // register footer preview to tt_content_drawFooter hook if this feature is active
+    if (empty($extensionConfiguration['hideFooterAnimationLabel']) || !$extensionConfiguration['hideFooterAnimationLabel']) {
+        TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('templates.typo3/cms-backend.partialsRootPaths = baschte/content-animations:Resources/Private/TemplateOverrides/typo3/cms-backend');
+    }
+});
